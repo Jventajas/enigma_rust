ba33d4127138918ca43ccbd8de212b368d7bb1a4
Javi Ventajas <jventajas.hdez@gmail.com>
2025-03-07 11:21:02 +0000

    Add form handling and encryption endpoint to web app
    Introduce a `Message` struct with form parsing to handle user input, and add a `/` POST route for encryption. This enhancement prepares for future encryption feature implementation by rendering encrypted and plain text in the template.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,25 @@
 
 use rocket_dyn_templates::{Template, context};
 use rocket::fs::{FileServer};
+use rocket::form::Form;
+
+#[derive(FromForm)]
+struct Message {
+    plaintext: String,
+    left_rotor: String,
+    left_initial_position: char,
+    left_ring_setting: char,
+    center_rotor: String,
+    center_initial_position: char,
+    center_ring_setting: char,
+    right_rotor: String,
+    right_initial_position: char,
+    right_ring_setting: char,
+    reflector: String,
+    plugboard: String,
+}
+
+
 
 #[get("/")]
 fn index() -> Template {
@@ -9,10 +28,29 @@ fn index() -> Template {
     Template::render("index", context! { alphabet: alphabet, name: "Rocket User" })
 }
 
+#[post("/", data = "<message>")]
+fn encrypt(message: Form<Message>) -> Template {
+    let alphabet: Vec<char> = ('A'..='Z').collect();
+    Template::render("index", context! {
+        alphabet: alphabet,
+        name: "Rocket User",
+        ciphertext: message.plaintext.clone(),
+        plaintext: message.plaintext.clone(),
+    })
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Template::fairing())
-        .mount("/", routes![index])
+        .mount("/", routes![index, encrypt])
         .mount("/static", FileServer::from("static"))
 }
+
+
+
+
+
+
+
+
